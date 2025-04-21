63fdb64c1e224120d72a2f621b4a70d11266fb0a
ImSpiDy <SpiDy2713@gmail.com>
2025-04-21 21:27:08 +0000

    reupload: Infinity on SFG
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- reupload.sh
+++ reupload.sh
@@ -7,13 +7,28 @@ git config --global user.email "SpiDy2713@gmail.com"
 echo $gh_token > mytoken.txt # login in github
 gh auth login --with-token < mytoken.txt
 
-# github release
-TAG=LineageExt-15-4.19
+# release
+SFG=1
+GHR=1
+DL_TAG=infinity-15-4.19
+UP_TAG=InfinityX-15-4.19
+VANILLA=Project_Infinity-X-2.8-lavender-20250509-1038-VANILLA-OFFICIAL.zip
+GAPPS=Project_Infinity-X-2.8-lavender-20250509-1226-GAPPS-OFFICIAL.zip
 
 # download tested builds
-gh release download $TAG -p 'lineage-22.1-Ext-Community-lavender-Vanilla-20250327-1253.zip' -R https://github.com/ImSpiDy/Test-Builds
-gh release download $TAG -p 'lineage-22.1-Ext-Community-lavender-Gapps-20250327-1321.zip' -R https://github.com/ImSpiDy/Test-Builds
+gh release download $DL_TAG -p $VANILLA -R https://github.com/ImSpiDy/Test-Builds
+gh release download $DL_TAG -p $GAPPS -R https://github.com/ImSpiDy/Test-Builds
 
-# upload release builds
-gh release create LosExt-15-4.19 --generate-notes --repo https://github.com/ImSpiDy/build-release
-gh release upload --clobber LosExt-15-4.19 *.zip --repo https://github.com/ImSpiDy/build-release
+if [ $SFG == 1 ]; then
+        if [ -f *VANILLA* ]; then
+                scp *VANILLA*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/vanilla/
+        fi
+        if [ -f *GAPPS* ]; then
+                scp *GAPPS*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/gapps/
+        fi
+fi
+if [ $GHR == 1 ]; then
+        # upload release builds
+        gh release create $UP_TAG --generate-notes --repo https://github.com/ImSpiDy/build-release
+        gh release upload --clobber $UP_TAG *.zip --repo https://github.com/ImSpiDy/build-release
+fi
