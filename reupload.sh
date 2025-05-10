f042e2008a829686cd0b6d872baaa04bcf1b0d2d
ImSpiDy <SpiDy2713@gmail.com>
2025-05-10 03:02:32 +0000

    reupload: use ssh to upload on SFG
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- reupload.sh
+++ reupload.sh
@@ -7,9 +7,20 @@ git config --global user.email "SpiDy2713@gmail.com"
 echo $gh_token > mytoken.txt # login in github
 gh auth login --with-token < mytoken.txt
 
+# login sfg (circle ci variable isn't working as intend"
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" > mysfgtoken.txt
+echo $SFG1 >> mysfgtoken.txt
+echo $SFG2 >> mysfgtoken.txt
+echo $SFG3 >> mysfgtoken.txt
+echo $SFG4 >> mysfgtoken.txt
+echo $SFG5 >> mysfgtoken.txt
+echo "-----END OPENSSH PRIVATE KEY-----" >> mysfgtoken.txt
+chmod 600 mysfgtoken.txt
+ssh-keyscan frs.sourceforge.net >> ~/.ssh/known_hosts
+
 # release
-SFG=1
-GHR=1
+SFG=0
+GHR=0
 DL_TAG=infinity-15-4.19
 UP_TAG=InfinityX-15-4.19
 VANILLA=Project_Infinity-X-2.8-lavender-20250509-1038-VANILLA-OFFICIAL.zip
@@ -21,10 +32,10 @@ gh release download $DL_TAG -p $GAPPS -R https://github.com/ImSpiDy/Test-Builds
 
 if [ $SFG == 1 ]; then
         if [ -f *VANILLA* ]; then
-                scp *VANILLA*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/vanilla/
+                scp -i mysfgtoken.txt *VANILLA*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/vanilla/
         fi
         if [ -f *GAPPS* ]; then
-                scp *GAPPS*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/gapps/
+                scp -i mysfgtoken.txt *GAPPS*.zip imspidy@frs.sourceforge.net:/home/frs/p/infinity-x/lavender/15/gapps/
         fi
 fi
 if [ $GHR == 1 ]; then
