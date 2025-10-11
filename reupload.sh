4198d6ca01d6e4bc54cf465f3cad624bc208cc62
ImSpiDy <SpiDy2713@gmail.com>
2025-10-11 12:19:47 +0000

    release infinity x3.3
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- reupload.sh
+++ reupload.sh
@@ -20,12 +20,12 @@ ssh-keyscan frs.sourceforge.net >> ~/.ssh/known_hosts
 
 # site
 SFG=1
-SFG_TAG=Yaap-16
+SFG_TAG=Infinity-X-16
 GHR=1
 
 # links
-VANILLA=https://github.com/ImSpiDy/Test-Builds/releases/download/YAAP-16-4.19/YAAP-16-HOMEMADE-VANILLA-lavender-20250929-1812.zip
-GAPPS=https://github.com/ImSpiDy/Test-Builds/releases/download/YAAP-16-4.19/YAAP-16-HOMEMADE-GAPPS-lavender-20250929-1826.zip
+VANILLA=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.3-lavender-20251010-1430-VANILLA-UNOFFICIAL.zip
+GAPPS=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.3-lavender-20251010-1518-GAPPS-UNOFFICIAL.zip
 
 if [ ! $VANILLA ]; then
 TAG=$(basename "$(dirname "$GAPPS")")
