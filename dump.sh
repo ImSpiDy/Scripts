4b056463439897a80fb5540735c59ee5036c44ca
ImSpiDy <SpiDy2713@gmail.com>
2025-12-26 13:20:17 +0000

    Import: Dumpyara Script
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

---
+++ dump.sh
@@ -0,0 +1,20 @@
+# Let's Dump
+
+# Dump folder
+DUMP="$(basename "$1" .zip)"
+
+# Install Dumpyara
+pipx install dumpyara
+
+# Download files
+DEPS=https://github.com/ImSpiDy/Scripts/releases/download/dump_deps/dump_deps.zip
+time aria2c $DEPS -x16 -s50
+time aria2c $1 -x16 -s50
+
+# Setup Deps
+unzip -o dump_deps.zip
+mv dump_deps/* $HOME/.local/bin
+export PATH="$HOME/.local/bin:$PATH"
+
+# Dump it
+dumpyara $DUMP.zip
