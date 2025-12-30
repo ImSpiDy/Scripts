adf79f2fdb22c4ff27242eee88d3943ef88679a0
ImSpiDy <SpiDy2713@gmail.com>
2025-12-30 08:28:16 +0000

    LosExt Update
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- reupload.sh
+++ reupload.sh
@@ -20,12 +20,14 @@ ssh-keyscan frs.sourceforge.net >> ~/.ssh/known_hosts
 
 # site # Infinity-X-16 # Yaap-16 #LosExt-16
 SFG=1
-SFG_TAG=Infinity-X-16
+#SFG_TAG=Infinity-X-16
+SFG_TAG=LosExt-16
+#SFG_TAG=Yaap-16
 GHR=1
 
 # links
-VANILLA=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.5-lavender-18.12.2025-VANILLA-UNOFFICIAL.zip
-GAPPS=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.5-lavender-18.12.2025-GAPPS-UNOFFICIAL.zip
+VANILLA=https://github.com/ImSpiDy/Test-Builds/releases/download/LineageExt-16-4.19/lineage-23.0-Ext-Community-lavender-Vanilla-20251229-1527.zip
+GAPPS=https://github.com/ImSpiDy/Test-Builds/releases/download/LineageExt-16-4.19/lineage-23.0-Ext-Community-lavender-Gapps-20251229-1539.zip
 
 if [ ! $VANILLA ]; then
 TAG=$(basename "$(dirname "$GAPPS")")
