8b063bf036976e2a287e61bbe3ff52f19da9c356
ImSpiDy <SpiDy2713@gmail.com>
2026-07-09 08:44:47 +0000

    lav: Infinity X3.12
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- lav_up.sh
+++ lav_up.sh
@@ -33,12 +33,12 @@ SFG=0
 #SFG_TAG=LosExt-16
 SFG_TAG=Yaap-16
 GHR=0
-DEV=0
-VEX=0
+DEV=1
+VEX=1
 
 # links
-VANILLA_URL=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.11-lavender-10.06.2026-VANILLA-UNOFFICIAL.zip
-GAPPS_URL=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.11-lavender-10.06.2026-GAPPS-UNOFFICIAL.zip
+VANILLA_URL=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.12-lavender-07.07.2026-VANILLA-UNOFFICIAL.zip
+GAPPS_URL=https://github.com/ImSpiDy/Test-Builds/releases/download/infinity-16-4.19/Project_Infinity-X-3.12-lavender-07.07.2026-GAPPS-UNOFFICIAL.zip
 
 if [ ! $VANILLA_URL ]; then
 	TAG=$(basename "$(dirname "$GAPPS_URL")")
